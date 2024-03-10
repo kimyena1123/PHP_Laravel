78deb012eb3783acc0ba208538bda0f41f8ca5d3
kimyena1123 <kyq104@gmail.com>
2024-03-10 12:01:44 +0900

    글 저장하기

--- routes/web.php
+++ routes/web.php
@@ -38,14 +38,45 @@ Route::get('/articles/create', function () {
 Route::post('/articles', function(Request $request){
     //비어있지 않고, 문자열이고, 255자를 넘으면 안된다.
     //방법1
-    $request->validate([
+    $input = $request->validate([
         'body' => [
             'required',
             'string',
             'max:255'
         ],
     ]);
+//    dd($input);
 
-    //글을 저장한다.
+    $host = config('database.connections.mysql.host');
+    $dbname = config('database.connections.mysql.database');
+    $username = config('database.connections.mysql.username');
+    $password = config('database.connections.mysql.password');
+//    dd($host);
+
+    //pdo 객체를 만들고 : $conn = new PDO("mysql:host=호스트명;dbnamae=데이터베이스", 사용자명, 패스워드);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    //쿼리를 준비하고
+    $stmt = $conn -> prepare("INSERT INTO articles (body, user_id) VALUES (:body, :userId);");
+//    dd($request->collect());
+    $body = $request->input('body');
+//    dd($body);
+
+    //사용자 아이디 가져오는 방법1
+    //dd($request->user()->id);
+
+    //사용자 아이디 가져오는 방법2
+    //dd(\Illuminate\Support\Facades\Auth::user()->id);
+
+    //사용자 아이디 가져오는 방법3
+    //dd(\Illuminate\Support\Facades\Auth::id());
+
+    //쿼리 값을 설정하고
+    $stmt->bindValue(':body', $input['body']);
+    $stmt->bindValue(':userId', $request->user()->id);
+
+    //실행
+    $stmt->execute();
+
     return 'hello';
 });
